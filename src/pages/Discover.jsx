aa343d4d089b13411fdc1e20888b96a4fdbfc590
Gal Oiring <74841666+galoiring@users.noreply.github.com>
2022-11-18 02:23:39 +0200

    addad genre funtionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,30 +1,36 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
+import { selectGenreListId } from "../redux/features/playerSlice";
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
 import { genres } from "../assets/constants";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
   const dispatch = useDispatch();
+  const { genreListId } = useSelector((state) => state.player);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = "Pop";
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
 
-  if (isFetching) return <Loader title="Loading Songs..." />;
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
+
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   return (
     <div className="flex flex-col">
-      <div
-        className="w-full flex justify-between items-center 
-      sm:flex-row flex-col mt-4 mb-10"
-      >
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">
           Discover {genreTitle}
         </h2>
+
         <select
-          onChange={() => {}}
-          value=""
-          className="bg-black text-grey-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
             <option key={genre.value} value={genre.value}>
@@ -33,6 +39,7 @@ const Discover = () => {
           ))}
         </select>
       </div>
+
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
           <SongCard
@@ -48,4 +55,5 @@ const Discover = () => {
     </div>
   );
 };
+
 export default Discover;
